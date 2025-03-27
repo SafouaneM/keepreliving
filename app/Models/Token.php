d0ae432d836e3c2e8d3c2a6d4cd9147d52a042a7
safouane.messaoudi <safouane.messaoudi@vodafoneziggo.com>
2025-03-27 22:40:21 +0100

    i want my sketches here

---
+++ app/Models/Token.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+
+    public function belongsTo()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function hasMany()
+    {
+        return $this->hasMany(Token::class);
+    }
 }
