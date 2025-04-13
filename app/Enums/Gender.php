e545fa57f2b09cae7e264a753d08fec5459c2f21
safouane.messaoudi <safouane.messaoudi@vodafoneziggo.com>
2025-04-13 16:12:44 +0200

    dustah fix

--- app/Enums/Gender.php
+++ app/Enums/Gender.php
@@ -3,19 +3,19 @@
 declare(strict_types=1);
 
 namespace App\Enums;
-enum Gender : string
+
+enum Gender: string
 {
     case male = 'male';
     case female = 'female';
     case preferNotToSay = 'prefer_not_to_say';
 
-
-    public function label() : string
+    public function label(): string
     {
         return match ($this) {
             self::male => 'Male',
             self::female => 'Female',
             self::preferNotToSay => 'Prefer not to say',
-       };
+        };
     }
 }
